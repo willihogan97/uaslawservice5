4931aee0183bfb512eb04251bab1eea8300652eb
William Adjandra <willihogan@gmail.com>
2019-05-25 09:33:46 +0700

    edit

--- server5/urls.py
+++ server5/urls.py
@@ -17,12 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-# from files.views import FilesMethods
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('', FilesMethods.method),
+    path('', views.sendTime),
 ]
-
-if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ server5/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import JsonResponse
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+import pika
+import datetime
+import time
+
+@csrf_exempt
+def sendTime(request):
+	credentials = pika.PlainCredentials('1506725003', '697670')
+	connection = pika.BlockingConnection(pika.ConnectionParameters('152.118.148.103',5672,'1506725003', credentials))
+	channel = connection.channel()
+	exchange = '1506725003_fanout'
+	channel.exchange_declare(exchange=exchange, exchange_type='fanout', passive=False, durable=False, auto_delete=False)
+
+	while(True) :
+		ts = datetime.datetime.today().strftime('%A, %d %B %Y, %H:%M:%S')
+		print("Jam : " + ts)
+		channel.basic_publish(exchange=exchange,
+							  routing_key='waktuServer',
+							  body=ts)
+		time.sleep(1)
+	connection.close()
+	return JsonResponse({"status": "ok"})
